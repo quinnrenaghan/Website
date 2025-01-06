046a42132e5f1ba4389799403a2a7d51f595f258
Quinn Renaghan <quinn@renaghan.org>
2025-01-05 22:14:38 -0500

    spacing

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -91,10 +91,10 @@ const ProjectsGrid = () => {
     ];
 
     return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-6xl mx-auto p-6 sm:p-2">
         <h1 className="text-4xl font-medium mb-6 text-rose-800 text-center">Project Archive</h1>
-        <div className="max-w-6xl mx-auto p-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="max-w-6xl mx-auto p-6 sm:p-2">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 sm:gap-4">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
