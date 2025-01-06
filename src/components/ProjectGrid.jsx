37a15d64271f3979d647d7ab63cfe1b9c92d1b7f
Quinn Renaghan <quinn@renaghan.org>
2025-01-05 22:21:58 -0500

    spacing

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -5,9 +5,9 @@ const ProjectCard = ({ project }) => {
 return (
     <a 
         href={project.link}
-        className="block aspect-square bg-paper border-2 border-gray-200 rounded-lg overflow-hidden hover:shadow-lg transition-shadow"
+        className="block bg-paper border-2 border-gray-200 rounded-lg overflow-hidden hover:shadow-lg transition-shadow"
     >
-    <div className="h-1/2 border-b">
+    <div className="h-48 border-b">
         <img 
             src={project.imageUrl} 
             alt={project.name}
@@ -17,7 +17,7 @@ return (
     
     <div className="p-4 h-1/2 flex flex-col">
         <div className="flex items-center justify-between mb-2">
-            <h3 className="text-lg font-medium text-rose-800 truncate">
+            <h3 className="break-words text-lg font-medium text-rose-800 truncate">
             {project.name}
         </h3>
             <ExternalLink className="w-4 h-4 text-black flex-shrink-0" />
@@ -91,10 +91,10 @@ const ProjectsGrid = () => {
     ];
 
     return (
-    <div className="max-w-6xl mx-auto p-6 sm:p-2">
+    <div className="max-w-6xl mx-auto p-4">
         <h1 className="text-4xl font-medium mb-6 text-rose-800 text-center">Project Archive</h1>
-        <div className="max-w-6xl mx-auto p-6 sm:p-2">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 sm:gap-4">
+        <div className="max-w-6xl mx-auto p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
                 ))}
