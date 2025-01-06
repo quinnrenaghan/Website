a2a6ac83309c40808a9ffb530a37b05b13087950
Quinn Renaghan <quinn@renaghan.org>
2025-01-05 22:12:26 -0500

    fixing spacing

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -91,9 +91,9 @@ const ProjectsGrid = () => {
     ];
 
     return (
-    <div className="max-w-6xl mx-auto p-12">
+    <div className="max-w-6xl mx-auto p-6">
         <h1 className="text-4xl font-medium mb-6 text-rose-800 text-center">Project Archive</h1>
-        <div className="max-w-6xl mx-auto p-12">
+        <div className="max-w-6xl mx-auto p-6">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
